367a71af367913aa083845f97ade45aa0215d9d8
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-07-26 15:36:37 -0400

    20180726

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // 1. When we click the button, run a function
 // 2. Inside the function, add/remove the class of open
 // 3. Make sure our link doesn't jump the page to the top
-
 $('.menu-toggle img').on('click', function(){
   $('.menu').toggleClass('open');
   return false;
