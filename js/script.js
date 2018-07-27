b023e42e3d6b481750467448df3a4df33d394d9b
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-07-27 00:59:30 -0400

    20180727

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ $('.menu-toggle img').on('click', function(){
   $('.menu').toggleClass('open');
   return false;
 });
+
+// get the window width every time when we change window size!
+$(function(){
+  $(window).resize(function(){
+      const windowWidth = $(window).width();
+      const responsiveHeight = windowWidth*2/3;
+      //console.log(responsiveHeight);
+      $('.home').css("height",responsiveHeight + "px");
+  }).resize();
+});
