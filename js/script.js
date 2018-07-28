82c05427b258939bc33eccf74f998e2fcb64d71a
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-07-28 00:53:26 -0400

    20180728

--- js/script.js
+++ js/script.js
@@ -6,12 +6,49 @@ $('.menu-toggle img').on('click', function(){
   return false;
 });
 
+$('.menu-link').on('click', function(){
+  $('.menu').toggleClass('open');
+  const href = $(this).attr('href');
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth'
+  });
+  return false;
+});
+
 // get the window width every time when we change window size!
 $(function(){
   $(window).resize(function(){
       const windowWidth = $(window).width();
-      const responsiveHeight = windowWidth*2/3;
+      const responsiveHeight = windowWidth*2/3-3;
       //console.log(responsiveHeight);
       $('.home').css("height",responsiveHeight + "px");
   }).resize();
+});
+
+// 1. whenever we click a .js-scroll link, we want to run a function
+// 2. we want to stop the link from jumping straight to our section (its default behaviour)
+// 3. we want to find out the href attribute, and then grab that element
+// 4. then scroll to it using scrollIntoView
+// const scrollLinks = document.querySelectorAll('.js-scroll');
+// scrollLinks.forEach(link => {
+//   link.addEventListener('click', (event)=>{
+//     // this is equivalent to return false in jQuery
+//     // it will block the default browser behaviour of the link jumping to the href attribute
+//     event.preventDefault();
+//     // here we grab the href attribute from our link
+//     const href = link.getAttribute('href');
+//     //console.log(href);
+//     document.querySelector(href).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+// ↓↓↓ jQuery way:
+$('.js-scroll').on('click',function(){
+  const href = $(this).attr('href');
+  //console.log(href);
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth'
+  });
+  return false;
 });
